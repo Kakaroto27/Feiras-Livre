2ea6dbeeba1ed7b90df2154ae607c782d39bc2be
André Carlos <115802574+Kakaroto27@users.noreply.github.com>
2023-08-11 14:30:53 -0300

    Add files via upload

--- src/pages/Filtro.tsx
+++ src/pages/Filtro.tsx
@@ -1,24 +1,62 @@
-import { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonDatetime, IonHeader, IonItem, IonLabel, IonPage, IonSelect, IonSelectOption, IonThumbnail, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButton, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonDatetime, IonHeader, IonInput, IonItem, IonLabel, IonList, IonListHeader, IonPage, IonSelect, IonSelectOption, IonThumbnail, IonTitle, IonToolbar } from "@ionic/react";
 import './Tab1.css';
 
 //imports do db//
 import { useEffect, useState } from "react";
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc, doc,deleteDoc, getDocs } from "firebase/firestore";
+import { getFirestore, collection, addDoc, doc, getDocs } from "firebase/firestore";
 
+    //banco de dados//
+    const firebase = initializeApp ({
+          apiKey: "AIzaSyD0EQ_WwexcLOuySG-PA5BEf7vfaFNqLYk",
+  authDomain: "afeira-f9ae6.firebaseapp.com",
+  projectId: "afeira-f9ae6",
+  storageBucket: "afeira-f9ae6.appspot.com",
+  messagingSenderId: "803345331495",
+  appId: "1:803345331495:web:535ef533e21e19c2a1a502"
+    });
 
-//banco de dados//
-const firebase = initializeApp( {
-    apiKey: "AIzaSyD0EQ_WwexcLOuySG-PA5BEf7vfaFNqLYk",
-    authDomain: "afeira-f9ae6.firebaseapp.com",
-    projectId: "afeira-f9ae6",
-    storageBucket: "afeira-f9ae6.appspot.com",
-    messagingSenderId: "803345331495",
-    appId: "1:803345331495:web:535ef533e21e19c2a1a502"
-  });
+    
 
 //Config do Export//
 const Filtro: React.FC = () => {
+
+    
+
+    /*Config do db*/
+
+
+    const [bairro, setbairro] = useState("");
+    const [dia, setdia] = useState("");
+    const [users, setfeiras] = useState([])
+ 
+    const db = getFirestore(firebase);
+    const userCollectionRef = collection(db, "users");
+
+    useEffect(() => {
+        const pegabairro = async () => {
+            const data = await getDocs(userCollectionRef);
+            setfeiras(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+        };
+        pegabairro();
+    });
+
+    async function criarDado() {
+        try {
+        const user = await addDoc(collection(db, "users"), {
+            bairro,
+            dia,
+        });
+
+        console.log("dados salvos com sucessos", bairro);
+        } catch (e) {
+        console.error("Error adding document: ", e);
+        }
+
+
+    }
+    
+
     return ( 
         <IonPage>
 
@@ -46,17 +84,36 @@ const Filtro: React.FC = () => {
                     
                     <IonItem className="caixaseleciona">
                         <IonSelect label="Escolha por Bairro" labelPlacement="floating" >
-                            
+                           <IonThumbnail><IonList></IonList></IonThumbnail> 
                         </IonSelect>
                     </IonItem>
                 {/*final do card de escolhas*/}
                 </IonCard>
+                
+                
+                {/*testes*/}
+                    <IonCard>
+                        <IonCardHeader>
+                            <IonLabel>testes</IonLabel>
+                        </IonCardHeader>
+                        <IonItem>
+                            <IonInput   type="text"
+                                        placeholder="Baiiro"
+                                        value={bairro}
+                                        onChange={(e) => setbairro(e.target.value)}
+                            />
+                            <IonInput   type="text"
+                                        placeholder="Diia"
+                                        value={dia}
+                                        onChange={(e) => setdia(e.target.value)}
+                            />
+                            <IonButton onClick={criarDado}>Criar dado</IonButton>
+                        </IonItem>
 
+                    </IonCard>
 
-                <IonCard>
-                {/*Card que aparece ultimos locais*/}
+                <IonCard>{/*Card que aparece ultimos locais*/}
                     <IonCardHeader>
-                        
                         <IonCardTitle className='titulocard' >
                             Feiras visitadas              
                         </IonCardTitle>
@@ -64,14 +121,14 @@ const Filtro: React.FC = () => {
                             Últimos locais que você visitou
                         </IonCardSubtitle>
                     </IonCardHeader>
+
                     <IonItem >
                         <IonThumbnail slot="start">
 
                         </IonThumbnail>
                         <IonLabel></IonLabel>
-                    </IonItem>
-                {/*final de 'ultimos locais'*/}    
-                </IonCard>
+                    </IonItem> 
+                </IonCard>{/*final de 'ultimos locais'*/}
 
             </IonContent>
         </IonPage>
@@ -80,31 +137,7 @@ const Filtro: React.FC = () => {
 }
 
 
-/*Config do db*/
-const [Bairro, setBairro] = useState("");
-const [dia, setdia] = useState("");
-const db = getFirestore(firebase);
-const userCollectionRef = collection(db, "filtrorj");
 
-async function criarDado() {
-    try {
-      const user = await addDoc(collection(db, "filtrorj"), {
-        Bairro,
-        dia,
-      });
-
-      console.log("dados salvos com sucessos", user);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  }
-    useEffect(() => {
-      const pegabairro = async () => {
-          const data = await getDocs(userCollectionRef);
-          setBairro(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-      };
-      pegabairro();
-    });
 
 export default Filtro;
 
